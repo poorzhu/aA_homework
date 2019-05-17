87465fac9088f66d3619c7235e64b1059b265638
Julian <thezhukeeper@gmail.com>
2019-05-16 22:57:06 -0700

    Create Stack class

--- W1D5/DIY_ADTs.rb
+++ W1D5/DIY_ADTs.rb
@@ -1,17 +1,19 @@
  class Stack
-    def initialize
-      # create ivar to store stack here!      
+    attr_reader :stack
+
+    def initialize    
+      @stack = []
     end
 
     def push(el)
-      # adds an element to the stack
+      @stack.push(el)
     end
 
     def pop
-      # removes one element from the stack
+      @stack.pop
     end
 
     def peek
-      # returns, but doesn't remove, the top element in the stack
+      @stack[-1]
     end
-  end
+end
