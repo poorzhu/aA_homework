88e57ded0873f961710ccf2d91b3ad04cb77fa7a
Julian <thezhukeeper@gmail.com>
2019-05-20 23:38:48 -0700

    Complete most of w2d2

--- W2D2/skeleton/lib/board.rb
+++ W2D2/skeleton/lib/board.rb
@@ -24,6 +24,7 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
+    # debugger
     stones = @cups[start_pos]
     @cups[start_pos] = []
     case current_player_name == @name1
@@ -54,10 +55,14 @@ class Board
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
 
+    @cups[ending_cup_idx]
+
     if @cups[ending_cup_idx].empty?
       return :switch
-    # elsif 
-    #   return :prompt
+    elsif ending_cup_idx == 6 || ending_cup_idx == 13
+      return :prompt
+    elsif !@cups[ending_cup_idx].empty?
+      return ending_cup_idx
     end
 
   end
@@ -71,8 +76,20 @@ class Board
   end
 
   def one_side_empty?
+    @cups[0..5].all?(&:empty?) || @cups[7..12].all?(&:empty?)
   end
 
   def winner
+    if @cups[6] == @cups[13]
+      return :draw
+    elsif one_side_empty?
+      debugger
+      case @cups[6] <=> @cups[13]
+      when -1
+        return @name2
+      when 1
+        return @name1
+      end
+    end
   end
 end
