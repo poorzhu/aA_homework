d26862ac7b30e4837bb0b21ac0062926c8aeab8f
Julian <thezhukeeper@gmail.com>
2019-05-20 23:15:33 -0700

    Initial commit, incomplete project

--- W2D2/skeleton/lib/board.rb
+++ W2D2/skeleton/lib/board.rb
@@ -1,7 +1,14 @@
+require "byebug"
+
 class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14) { [:stone, :stone, :stone, :stone] }
+    @cups[6] = []
+    @cups[13] = []
   end
 
   def place_stones
@@ -9,13 +16,50 @@ class Board
   end
 
   def valid_move?(start_pos)
+    if start_pos < 0 || start_pos > 13
+      raise ArgumentError.new "Invalid starting cup"
+    elsif @cups[start_pos].empty?
+      raise ArgumentError.new "Starting cup is empty"
+    end
   end
 
   def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
+    case current_player_name == @name1
+    when true
+      other_player_cup = 13
+    when false
+      other_player_cup = 6
+    end
+
+    if start_pos == other_player_cup - 1
+      start_pos = (start_pos + 2) % @cups.length
+    else
+      start_pos = (start_pos + 1) % @cups.length
+    end
+
+    until stones.empty?
+      @cups[start_pos] << stones.shift if start_pos != other_player_cup
+      if !stones.empty?
+        start_pos = (start_pos + 1) % @cups.length
+      end
+    end
+
+    render
+    # debugger
+    next_turn(start_pos)
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+
+    if @cups[ending_cup_idx].empty?
+      return :switch
+    # elsif 
+    #   return :prompt
+    end
+
   end
 
   def render
